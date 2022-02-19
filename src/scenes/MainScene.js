93e22c430b747e8317e3534d0ddcd222ea34cf3c
Kosi Nwabueze <kosinwabueze@gmail.com>
2022-02-19 03:19:29 -0500

    tilemaps

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -5,6 +5,9 @@ import backgroundImg from "../assets/images/background.png";
 import playerSheet from '../assets/sprites/player/sheet.png';
 import playerAtlas from '../assets/sprites/player/atlas.json';
 
+import tilemap01 from '../assets/tilemaps/01.json';
+import tileset01 from '../assets/tilesets/01.png';
+
 import Background from '../components/Background';
 import Player from '../components/Player';
 
@@ -19,6 +22,41 @@ export default class MainScene extends Phaser.Scene {
     // Place your code to load assets here...
     this.load.image('background', backgroundImg);
     this.load.atlas('player', playerSheet, playerAtlas);
+
+    // Loading in tilemap assets.
+    this.load.tilemapTiledJSON('tilemap/01', tilemap01);
+    this.load.image('tileset/01', tileset01);
+  }
+
+  /**
+ * Draws debug graphics for layer.
+ * 
+ * @param {Phaser.Tilemaps.TilemapLayer} layer 
+ */
+  debugTilemapLayer(layer) {
+    const gfx = this.add.graphics();
+
+    layer.renderDebug(gfx, {
+      collidingTileColor: null,
+      tileColor: null,
+      faceColor: new Phaser.Display.Color(255, 255, 0, 255)
+    });
+  }
+
+  createTilemap() {
+    const map = this.make.tilemap({ key: 'tilemap/01' });
+    const tileset = map.addTilesetImage('01', 'tileset/01');
+    const foreground = map.createLayer('foreground', tileset, 0, 0);
+
+    // Tell Phaser to make every tile on 'foreground' layer collideable.
+    foreground.setCollisionByExclusion([-1]);
+
+    // this.debugTilemapLayer(foreground);
+
+    return {
+      map,
+      foreground
+    };
   }
 
   create() {
@@ -33,6 +71,23 @@ export default class MainScene extends Phaser.Scene {
     const y = this.cameras.main.centerY;
 
     this.player = new Player(this, x, y);
+
+    // Initialize our tilemap.
+    this.tilemap = this.createTilemap();
+
+    // Set the world bounds to be the dimensions of the tilemap.
+    this.physics.world.setBounds(0, 0, this.tilemap.map.widthInPixels, this.tilemap.map.heightInPixels);
+
+    // Setup collisions between the player and the tilemap.
+    this.physics.add.collider(this.player, this.tilemap.foreground);
+
+    // Tell the camera to follow the player and set world bounds to be tilemap dimensions.
+    const { main } = this.cameras;
+    main.startFollow(this.player);
+    main.setBounds(0, 0, this.tilemap.map.widthInPixels, this.tilemap.map.heightInPixels);
+
+    // Tell background to not scroll with the camera.
+    this.background.setScrollFactor(0);
   }
 
   update() {
